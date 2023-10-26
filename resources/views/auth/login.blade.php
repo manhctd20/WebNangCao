44f1844f4147fc99bf6b16278a67e9a5b7a5e8df
phamdung2107 <phamdung21072002@gmail.com>
2023-10-26 16:43:37 +0700

    fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card mt-3 mb-3">
+                <div class="card-header mb-3">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -16,7 +16,7 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 mb-3">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -30,7 +30,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 mb-3">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -42,7 +42,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 mb-3 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -50,8 +50,14 @@
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
+                                <div class="col-md-8 offset-md-4">
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Bạn chưa có tài khoản?') }}
+                                    </a>
+                                </div>
                             </div>
                         </div>
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
@@ -61,7 +67,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Bạn quên mật khẩu?') }}
                                     </a>
                                 @endif
                             </div>
